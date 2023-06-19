74722f3ad4c199e0d10185355cd7e2a6f79607b9
Chaman Modi <59691163+modi295@users.noreply.github.com>
2023-06-19 14:45:32 +0530

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,6 @@ export const environment = {
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
     XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: '99fb7f5c16mshfd2a6e7be104d41p1c1dfcjsnd542d0d6e243'
+    XRapidAPIKeyHeaderValue: 'use your api key'
     
 };
