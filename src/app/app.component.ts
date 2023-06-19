2e903ba17fb7ceac4e6cd391ca4753a23646f9d1
modi295 <chamanmodi19@gmail.com>
2023-06-19 11:16:12 +0530

    modified:   angular.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/models/weather.model.ts 	new file:   src/app/services/weather.service.spec.ts 	new file:   src/app/services/weather.service.ts 	new file:   src/assets/cold.jpg 	new file:   src/assets/hot.jpg 	new file:   src/assets/humity.jpg 	new file:   src/assets/max.png 	new file:   src/assets/min.jpg 	new file:   src/assets/wind.png 	new file:   src/environments/environment.development.ts 	new file:   src/environments/environment.ts 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  constructor(private weatherServices: WeatherService){
+
+  }
+  cityName: string='delhi';
+  weatherData? :WeatherData;
+
+  ngOnInit(): void {
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+  }
+  onSubmit(){
+    this.getWeatherData(this.cityName);
+    this.cityName='';
+
+  
+  }
+  private getWeatherData(cityName:string){
+    this.weatherServices.getWeatherData(cityName)
+    .subscribe({
+      next: (response) =>{
+        this.weatherData=response;
+        console.log(response);
+      }
+    })
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: '99fb7f5c16mshfd2a6e7be104d41p1c1dfcjsnd542d0d6e243'
+};
